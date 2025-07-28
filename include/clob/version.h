8ce37198696b3c4c5636934d794365e6f0b96aa4
Nathanael Lu <lunathanael@gmail.com>
2025-07-28 01:41:55 -0400

    upgrade version to 0.1.0

--- include/clob/version.h
+++ include/clob/version.h
@@ -10,9 +10,9 @@
 
 namespace clob {
 /** Version Info **/
-constexpr uint32_t VERSION_MAJOR{10};
-constexpr uint32_t VERSION_MINOR{0};
-constexpr uint32_t VERSION_PATCH{1};
+constexpr uint32_t VERSION_MAJOR{0};
+constexpr uint32_t VERSION_MINOR{1};
+constexpr uint32_t VERSION_PATCH{0};
 constexpr uint32_t VERSION{VERSION_MAJOR * 10000 + VERSION_MINOR * 100 +
                            VERSION_PATCH};
 } // namespace clob
