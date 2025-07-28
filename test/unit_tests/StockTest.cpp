b60c40c6a6590f4b1572b6f2936d96b99cb41bf4
Nathanael Lu <lunathanael@gmail.com>
2025-07-28 17:10:45 -0400

    test: add unit tests for

--- test/unit_tests/StockTest.cpp
+++ test/unit_tests/StockTest.cpp
@@ -1,5 +1,9 @@
 #include "doctest/doctest.h"
 
+#include <cstdint>
+#include <type_traits>
+#include <utility>
+
 #include "misc/TestUtilities.h"
 
 #include "clob/Stock.h"
@@ -28,6 +32,33 @@ TEST_CASE("stock_move_constructor") {
   CHECK(stock2.name == "stock1");
   CHECK(stock2.ticker == "AAPL");
   CHECK(stock2.id == 1);
+}
+
+/***/
+TEST_CASE("stock_copy_constructors") {
+  Stock stock1{"stock1", "AAPL", 1};
+  Stock stock2{stock1};
+  CHECK(stock2.name == "stock1");
+  CHECK(stock2.ticker == "AAPL");
+  CHECK(stock2.id == 1);
+}
+
+/***/
+TEST_CASE("stock_types") {
+  Stock stock{"stock", "AAPL", 2};
+
+  REQUIRE(stock.name == "stock");
+  REQUIRE(stock.ticker == "AAPL");
+  REQUIRE(stock.id == 2);
+
+  CHECK(std::is_same_v<decltype(stock.id), const Stock::id_t>);
+  CHECK(sizeof(stock.id) >= sizeof(int32_t));
+}
+
+/***/
+TEST_CASE("stock_assignment_deleted") {
+    CHECK_FALSE(std::is_copy_assignable_v<Stock>);
+    CHECK_FALSE(std::is_move_assignable_v<Stock>);
 }
 
 TEST_SUITE_END();
