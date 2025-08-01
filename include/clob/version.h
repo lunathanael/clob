7ef5eb5bccde95cbcb5a983b0071097d7f791a87
Nathanael Lu <lunathanael@gmail.com>
2025-08-01 04:52:13 -0400

    chore: up version

--- include/clob/version.h
+++ include/clob/version.h
@@ -11,7 +11,7 @@
 namespace clob {
 /** Version Info **/
 constexpr uint32_t VERSION_MAJOR{0};
-constexpr uint32_t VERSION_MINOR{2};
+constexpr uint32_t VERSION_MINOR{3};
 constexpr uint32_t VERSION_PATCH{0};
 constexpr uint32_t VERSION{VERSION_MAJOR * 10000 + VERSION_MINOR * 100 +
                            VERSION_PATCH};
